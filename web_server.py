5afc951e91fedf21a7116e7d8e396e8ba406454a
ruohl <rudolfgbohl@hotmail.com>
2024-06-04 22:06:47 -0300

    Modifico código para que aparezca el nombre de los clientes conectados

--- web_server.py
+++ web_server.py
@@ -6,31 +6,34 @@ from dhcpv6_server import start_dhcpv6_server
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# Lista compartida entre los clientes conectados
-connected_clients = []
+# Diccionario compartido entre los clientes conectados, con IP como clave
+connected_clients = {}
 
 @app.route('/')
 def index():
-    return render_template('index.html', clients=connected_clients)
+    return render_template('index.html', clients=connected_clients.values())
 
 @socketio.on('connect')
 def handle_connect():
     client_ip = request.remote_addr
     if client_ip not in connected_clients:
-        connected_clients.append(client_ip)
-    emit('update_clients', connected_clients, broadcast=True)
+        connected_clients[client_ip] = {'ip': client_ip, 'username': 'Anonymous'}
+    emit('update_clients', list(connected_clients.values()), broadcast=True)
 
 @socketio.on('disconnect')
 def handle_disconnect():
     client_ip = request.remote_addr
     if client_ip in connected_clients:
-        connected_clients.remove(client_ip)
-    emit('update_clients', connected_clients, broadcast=True)
+        del connected_clients[client_ip]
+    emit('update_clients', list(connected_clients.values()), broadcast=True)
 
 @socketio.on('message')
 def handle_message(msg):
-    print(f"Message: {msg}")
+    client_ip = request.remote_addr
+    if client_ip in connected_clients:
+        connected_clients[client_ip]['username'] = msg['username']
     send(msg, broadcast=True)
+    emit('update_clients', list(connected_clients.values()), broadcast=True)
 
 if __name__ == "__main__":
     dhcp_thread = threading.Thread(target=start_dhcpv6_server, args=(connected_clients,))
